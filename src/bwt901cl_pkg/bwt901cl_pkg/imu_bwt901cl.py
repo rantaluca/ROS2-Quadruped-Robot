3cd22d653326a76cf4b862990f501b975101f43c
rob <dev@antaluca.com>
2024-07-24 11:11:24 +0200

    imu

--- src/bwt901cl_pkg/bwt901cl_pkg/imu_bwt901cl.py
+++ src/bwt901cl_pkg/bwt901cl_pkg/imu_bwt901cl.py
@@ -3,18 +3,19 @@ from rclpy.node import Node
 from sensor_msgs.msg import Imu, Temperature, MagneticField
 from geometry_msgs.msg import Vector3
 import subprocess
+
 from .src.bwt901cl import BWT901CL
 
 class Imu901cl(Node):
     def __init__(self, time_interval=1.0):
         super().__init__('imu_bwt901cl')
-        self.pub_imu = self.create_publisher(Imu, '/imu/data', 10)
-        self.pub_mag = self.create_publisher(MagneticField, '/imu/mag', 10)
-        self.pub_tmp = self.create_publisher(Temperature, '/imu/temp', 10)
-        self.pub_ang = self.create_publisher(Vector3, '/imu/angle', 10)
+        self.pub_imu = self.create_publisher(Imu, '/sensor/bwt901cl/Imu', 10)
+        self.pub_mag = self.create_publisher(MagneticField, '/sensor/bwt901cl/MagneticField', 10)
+        self.pub_tmp = self.create_publisher(Temperature, '/sensor/bwt901cl/Temperature', 10)
+        self.pub_ang = self.create_publisher(Vector3, '/sensor/bwt901cl/Angle', 10)
         self.tmr = self.create_timer(time_interval, self.timer_callback)
-        subprocess.call("sudo chmod 777 /dev/ttyUSB1", shell=True)
-        self.imu_sensor = BWT901CL("/dev/ttyUSB1")
+        subprocess.call("sudo chmod 777 /dev/ttyUSB0", shell=True)
+        self.imu_sensor =  BWT901CL("/dev/ttyUSB0")
 
     def timer_callback(self):
         msg_imu = Imu()
@@ -24,7 +25,7 @@ class Imu901cl(Node):
 
         angle, angular_velocity, accel, temp, magnetic, quaternion, time = self.imu_sensor.getData()
 
-        msg_tmp.temperature = temp
+        msg_tmp.temperature = float(temp)
         self.pub_tmp.publish(msg_tmp)
 
         msg_mag.magnetic_field.x = float(magnetic[0])
@@ -32,20 +33,16 @@ class Imu901cl(Node):
         msg_mag.magnetic_field.z = float(magnetic[2])
         self.pub_mag.publish(msg_mag)
 
-        # Ensure quaternion values are correctly set
         msg_imu.orientation.x = float(quaternion[0])
         msg_imu.orientation.y = float(quaternion[1])
         msg_imu.orientation.z = float(quaternion[2])
         msg_imu.orientation.w = float(quaternion[3])
-        
         msg_imu.angular_velocity.x = float(angular_velocity[0])
         msg_imu.angular_velocity.y = float(angular_velocity[1])
         msg_imu.angular_velocity.z = float(angular_velocity[2])
-        
         msg_imu.linear_acceleration.x = float(accel[0])
-        msg_imu.linear_acceleration.y = float(accel[1])
+        msg_imu.linear_acceleration.y = float(ccel[1])
         msg_imu.linear_acceleration.z = float(accel[2])
-        
         self.pub_imu.publish(msg_imu)
 
         msg_ang.x = float(angle[0])
@@ -53,10 +50,21 @@ class Imu901cl(Node):
         msg_ang.z = float(angle[2])
         self.pub_ang.publish(msg_ang)
 
-def main(args=None):
+        #print("Time:", time)
+        #print("th:", angle)
+        #print("d_th: ", angular_velocity)
+        #print("d_x: ", accel)
+        #print("mag: ", magnetic)
+        #print("tmp: ", temp)
+        #print(quaternion)
+
+def main(args=None):    
+    print('Hi from bwt901cl_pkg.')
+
     rclpy.init(args=args)
     node_imu_bwt901cl = Imu901cl(time_interval=0.1)
     rclpy.spin(node_imu_bwt901cl)
+
     node_imu_bwt901cl.destroy_node()
     rclpy.shutdown()
 
